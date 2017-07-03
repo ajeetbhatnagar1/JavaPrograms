a92bf86698058c4d6546515118a2b43c16023741
ajeetbhatnagar1 <ajeet.bhatnagar@hotmail.com>
2017-07-03 17:22:40 +0800

    init Commit

---
+++ src/com/java/ajeet/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.java.ajeet;
+
+/**
+ * Simple Hello World Program.
+ * 
+ * @author ajeet.a.bhatnagar
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World...");
+	}
+}
