aa426c8dc827fbbb3fa354a898ebd888e4177fa2
ajeetbhatnagar1 <ajeet.bhatnagar@hotmail.com>
2017-08-13 23:14:51 +0800

    My changes

--- src/com/java/ajeet/HelloWorld.java
+++ src/com/java/ajeet/HelloWorld.java
@@ -10,6 +10,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World...");
-		System.out.println("Hello i am Aj");
+		System.out.println("Hello i am Ajeet");
 	}
 }
